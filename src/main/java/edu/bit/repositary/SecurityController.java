7acc39d8695bda2c77a64decad7785d46e7914e5
rr_inhye <inhye1028@gmail.com>
2020-08-25 10:38:40 +0900

    controller 추가

---
+++ src/main/java/edu/bit/repositary/SecurityController.java
@@ -0,0 +1,43 @@
+package edu.bit.repositary;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+//develope-5
+//develope-4
+//develope-3
+//develope-2
+@Controller
+public class SecurityController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SecurityController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
